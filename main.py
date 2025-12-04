beb51ae3af69577932c74e710478e29acaf991dc
chloeleang-dev <chloe.leang@edu.esiee.fr>
2025-12-04 10:34:46 +0000

    tuples

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+"""tuples"""
 #### Imports et définition des variables globales
 
 # Mandatory for the recursive solution to work on large inputs
 import sys
 sys.setrecursionlimit(2000)
 
-
 #### Fonctions secondaires
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères 
+       passée en argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -17,14 +18,30 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
+
     # votre code ici
+    a = 1
+    l = []
+    ld = []
 
-    return [ ]
+    for i in range(1, len(s)):
+        if s[i-1] == s[i]:
+            print(i)
+            a += 1
+        else:
+            l.append(a) # nombre de lettre en commun
+            ld.append(s[i-1]) # carractère
+            a = 1
 
+    # Ajouter le dernier groupe
+    l.append(a)
+    ld.append(s[-1])
+
+    return list(zip(ld, l))
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne de caractères 
+       passée en argument selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -32,20 +49,23 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
+
     # votre code ici
-
     # cas de base
+    if s == "":
+        return []
     # recherche nombre de caractères identiques au premier
-    # appel récursif
+    a = 1
+    while a < len(s) and s[a] == s[0]:
+        a += 1
 
-    return []
-    
+    # appel récursif
+    return [(s[0], a)] + artcode_r(s[a:])
 
 #### Fonction principale
 
-
 def main():
+    """main"""
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
